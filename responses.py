ed40c2c78d035eef423558abe92a63a80e64c2e2
Wilson Chen <wilsonchen901@gmail.com>
2023-05-16 10:32:00 -0400

    start response file

---
+++ responses.py
@@ -0,0 +1,14 @@
+import random
+
+def handle_response(message):
+    
+    p_message = message.lower()
+    
+    if p_message == 'hello':
+        return "Was crackin cuz"
+    
+    if p_message == 'roll':
+        return str(random.randint(1,6))
+    
+    if p_message == 'help':
+        return "Figure it out yourself dummy"
